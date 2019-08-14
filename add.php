65e01c074b033eda5a38b0c004c6c766659bd252
Marta Kaszuba <48859858+martakaszuba@users.noreply.github.com>
2019-08-14 18:05:03 +0200

    Add files via upload

--- add.php
+++ add.php
@@ -1,65 +1,62 @@
 <?php
 session_start();
-$price = $_POST["price"];
-$title = $_POST["title"];
-$author = $_POST["author"];
-$img = $_POST["image"];
-
-$conn = new mysqli("localhost", "root", "", "ksiazki");
-		$conn->set_charset("utf8");
-		if ($conn->connect_error) {
-			die ("Connection failed: " . $conn->connect_error);
-		}
-
-    $stmtpre = $conn->prepare("SELECT * FROM bazaksiazki WHERE tytul =?");
-    $stmtpre->bind_param("s", $title);
-    $stmtpre->execute();
-    $result = $stmtpre->get_result();
-    if ($result->num_rows === 0) {
-        echo 'Nie ma takiego wyniku!';
-        $stmtpre->close();
-        $conn->close();
-        die;
-    }
-    else {
-      while ($row = $result->fetch_assoc()) {
-        if ($row["autor"] !== $author){
-          echo "nie ma takiego autora!";
-          die;
-        }
-        else if ($row['cena'] != $price){
-          echo "nie ma takiej ceny!";
-          die;
-        }
-        else {
-          echo "wszystko sie zgadza!";
-        }
+if (isset($_POST["price"]) && isset($_POST["title"]) && isset($_POST["author"]) && isset($_POST["image"])){
+  $price = $_POST["price"];
+  $title = $_POST["title"];
+  $author = $_POST["author"];
+  $img = $_POST["image"];
+  
+  $conn = new mysqli("localhost", "root", "", "ksiazki");
+      $conn->set_charset("utf8");
+      if ($conn->connect_error){
+        die ("Connection failed: " . $conn->connect_error);
       }
-      $stmtpre->close();
-        $conn->close();
-    }
-
-if (!isset($_SESSION["arr"])){
-  $_SESSION["arr"] = [];
-  $_SESSION["arr"][] = [$img,$title,$author, $price];
-}
-
-else {
-  foreach ($_SESSION["arr"] as $item){
-    if ($title === $item[1]){
-      die;
-    }
+  
+      $stmtpre = $conn->prepare("SELECT * FROM bazaksiazki WHERE tytul =?");
+      $stmtpre->bind_param("s", $title);
+      $stmtpre->execute();
+      $result = $stmtpre->get_result();
+      if ($result->num_rows === 0) {
+          $stmtpre->close();
+          $conn->close();
+          die;
+      }
+      else {
+        while ($row = $result->fetch_assoc()) {
+          if ($row["autor"] !== $author){
+            die;
+          }
+          else if ($row['cena'] != $price){
+            die;
+          }
+        }
+        $stmtpre->close();
+          $conn->close();
+      }
+  
+  if (!isset($_SESSION["arr"])){
+    $_SESSION["arr"] = [];
+    $_SESSION["arr"][] = [$img, $title, $author, $price];
   }
-  $_SESSION["arr"][] = [$img,$title,$author, $price];
+  
+  else {
+    foreach ($_SESSION["arr"] as $item){
+      if ($title === $item[1]){
+        die;
+      }
+    }
+    $_SESSION["arr"][] = [$img,$title,$author, $price];
+  }
+  
+  if (!isset($_SESSION["count"])){
+    $_SESSION["count"] =0;
+    $_SESSION["count"]+=$price;
+  }
+  
+  else {
+    $_SESSION["count"] +=$price;
+  }
 }
 
-if (!isset($_SESSION["count"])){
-  $_SESSION["count"] =0;
-  $_SESSION["count"]+=$price;
-}
-
-else {
-  $_SESSION["count"] +=$price;
-}
 
 ?>
