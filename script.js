c8d27396bcd2fd074d30555254ffe69fa0d02190
Marta Kaszuba <48859858+martakaszuba@users.noreply.github.com>
2019-08-17 14:43:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", Load);
         var num = Number(e.target.parentNode.querySelector(".price").innerHTML.slice(0,-2));
         var title = e.target.parentNode.querySelector(".title").innerHTML;
         var author = e.target.parentNode.querySelector(".author").innerHTML;
-        author = author.replace("Autor: ",");
+        author = author.replace("Autor: ","");
         var img = e.target.parentNode.querySelector("img").getAttribute("src");
           $.ajax({
             method:"post",
